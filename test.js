a32753d204f9226cf10cb4c0b40603c4452a0ce4
Martin Schneider <martin.schneider@hfg-gmuend.de>
2019-12-04 14:13:55 +0100

    Indent 4 spaces

--- test.js
+++ test.js
@@ -3,24 +3,24 @@ const util = require("util");
 const { createPlan } = require("./plan");
 
 const teams = [
-  "Im Rollstuhl einkaufen",
-  "Plötzlicher Kindstod",
-  "Variomorphe Bedienelemente",
-  "Sicher nach Hause kommen",
-  "Tattoo-UI",
-  "Haut",
-  "Smarte Kontaktlinsen",
-  "Smarte Ohrstöpsel",
-  "AD-Wandler",
-  "Legasthenie"
+    "Im Rollstuhl einkaufen",
+    "Plötzlicher Kindstod",
+    "Variomorphe Bedienelemente",
+    "Sicher nach Hause kommen",
+    "Tattoo-UI",
+    "Haut",
+    "Smarte Kontaktlinsen",
+    "Smarte Ohrstöpsel",
+    "AD-Wandler",
+    "Legasthenie"
 ];
 
 // log to file ...
 var log_file = fs.createWriteStream("beispiel.md", { flags: "w" });
 var log_stdout = process.stdout;
 console.log = function(d) {
-  log_file.write(util.format(d) + "\n");
-  log_stdout.write(util.format(d) + "\n");
+    log_file.write(util.format(d) + "\n");
+    log_stdout.write(util.format(d) + "\n");
 };
 
 // demo
